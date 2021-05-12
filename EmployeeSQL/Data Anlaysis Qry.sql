229430b5b960926fc5453a6880f58a4f4eacacdc
Amy's laptop <awieser@readyfoods.biz>
2021-05-11 19:23:14 -0600

    Add Data Analysis Qry 6-8

--- EmployeeSQL/Data Anlaysis Qry.sql
+++ EmployeeSQL/Data Anlaysis Qry.sql
@@ -29,7 +29,21 @@ WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%';
 
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Development' OR dept_name = 'Sales';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT employees.last_name, COUNT(employees.last_name) AS "Count last_name"
+FROM employees
+GROUP BY employees.last_name
+ORDER BY "Count last_name" DESC;
